756e2650a2b8c4d76d95b20accaa34dad34bcc7d
amal ouassit <amal.ouassit@bobst.com>
2024-09-25 19:31:53 +0100

    startting-ex04 fix message

--- • Python - 0 - Starting/ex04/whatis.py
+++ • Python - 0 - Starting/ex04/whatis.py
@@ -5,8 +5,8 @@ argument = sys.argv[1]
 try:
     assert len(sys.argv) == 2, "more than one argument is provided"
     assert argument.isdigit(), "argument is not an integer"
-except AssertionError as e:
-    print(e)
+except AssertionError as error:
+    print(AssertionError.__name__ + ":", error)
     sys.exit(1)
 if (int(argument) % 2) == 0:
     print("I'm Even.")
